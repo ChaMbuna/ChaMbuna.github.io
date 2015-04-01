70db243cda1e78dca786774e5811b403aec75159
Maarten <maarten.cappaert@gmail.com>
2015-04-01 13:30:03 -0500

    v1.3
    TODO: copy readme and upload to live server

---
+++ js/model.js
@@ -0,0 +1,42 @@
+var locationData = [
+    {
+        name: "Spider-Man",
+        address: "Forest Hills, New York",
+        id: 1009610
+    },
+    {
+        name: "Wolverine",
+        address: "Alberta, Canada",
+        id: 1009718
+    },
+    {
+        name: "Iron Man",
+        address: "Long Island, New York",
+        id: 1009368
+    },
+    {
+        name: "3-D Man",
+        address: "Los Angeles, California",
+        id: 1011334
+    },
+    {
+        name: "Captain America",
+        address: "New York, New York",
+        id: 1009220
+    },
+    {
+        name: "Hulk",
+        address: "Dayton, Ohio",
+        id: 1009351
+    },
+    {
+        name: "Black Widow",
+        address: "Stalingrad, Russia",
+        id: 1009189
+    },
+    {
+        name: "Red Skull",
+        address: "Germany",
+        id: 1009535
+    }
+];
